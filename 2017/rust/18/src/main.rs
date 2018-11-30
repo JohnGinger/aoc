ce6cfbeecb055b965a8ba4a60ba4b5377446e628
John Ginger <JohnGinger@users.noreply.github.com>
2018-11-30 00:08:59 +0000

    Minor tidyup

--- 2017/rust/18/src/main.rs
+++ 2017/rust/18/src/main.rs
@@ -5,7 +5,7 @@ use std::thread;
 use std::time::Duration;
 
 #[derive(Debug, Clone)]
-enum Instructions {
+enum Command {
     Snd,
     Set,
     Add,
@@ -17,20 +17,20 @@ enum Instructions {
 
 #[derive(Debug, Clone)]
 struct InstructionLine {
-    command: Instructions,
+    command: Command,
     register: char,
     value: (Option<i64>, char),
 }
 
-fn get_instruction(instruction: &str) -> Instructions {
+fn get_instruction(instruction: &str) -> Command {
     match instruction {
-        "snd" => Instructions::Snd,
-        "set" => Instructions::Set,
-        "add" => Instructions::Add,
-        "mul" => Instructions::Mul,
-        "mod" => Instructions::Mod,
-        "rcv" => Instructions::Rcv,
-        "jgz" => Instructions::Jgz,
+        "snd" => Command::Snd,
+        "set" => Command::Set,
+        "add" => Command::Add,
+        "mul" => Command::Mul,
+        "mod" => Command::Mod,
+        "rcv" => Command::Rcv,
+        "jgz" => Command::Jgz,
         _ => panic!("Strange instruction"),
     }
 }
@@ -80,24 +80,24 @@ fn part1(instructions: &Vec<InstructionLine>) {
         let register_value = get_register_value(&registers, instruction.register);
         let value = get_value(&registers, instruction.value);
         match instruction.command {
-            Instructions::Set => registers.insert(instruction.register, value),
-            Instructions::Add => registers.insert(instruction.register, register_value + value),
-            Instructions::Mul => registers.insert(instruction.register, register_value * value),
-            Instructions::Mod => registers.insert(instruction.register, register_value % value),
-            Instructions::Snd => {
+            Command::Set => registers.insert(instruction.register, value),
+            Command::Add => registers.insert(instruction.register, register_value + value),
+            Command::Mul => registers.insert(instruction.register, register_value * value),
+            Command::Mod => registers.insert(instruction.register, register_value % value),
+            Command::Snd => {
                 played = register_value;
                 None
             }
-            Instructions::Rcv => {
+            Command::Rcv => {
                 if register_value != 0 {
                     println!("Part 1 is {}", played);
                     break;
                 }
                 None
             }
-            Instructions::Jgz => {
+            Command::Jgz => {
                 if register_value > 0 {
-                    position = position + value - 1;
+                    position += value - 1;
                 }
                 None
             }
@@ -135,33 +135,29 @@ impl DuetPlayingProgram {
         let mut sent = 0;
         while self.position >= 0 && self.position < self.instructions.len() as i64 {
             let instruction = &self.instructions[self.position as usize];
-            let register_value = get_register_value(&self.registers, instruction.register);
+            let register = instruction.register;
+            let register_value = get_register_value(&self.registers, register);
+
             let value = get_value(&self.registers, instruction.value);
             match instruction.command {
-                Instructions::Set => self.registers.insert(instruction.register, value),
-                Instructions::Add => self
-                    .registers
-                    .insert(instruction.register, register_value + value),
-                Instructions::Mul => self
-                    .registers
-                    .insert(instruction.register, register_value * value),
-                Instructions::Mod => self
-                    .registers
-                    .insert(instruction.register, register_value % value),
-                Instructions::Snd => {
+                Command::Set => self.registers.insert(register, value),
+                Command::Add => self.registers.insert(register, register_value + value),
+                Command::Mul => self.registers.insert(register, register_value * value),
+                Command::Mod => self.registers.insert(register, register_value % value),
+                Command::Snd => {
                     self.sender
                         .send(register_value)
                         .expect("There wasn't something to send to");
                     sent += 1;
                     None
                 }
-                Instructions::Rcv => match self.receiver.recv_timeout(Duration::from_millis(100)) {
+                Command::Rcv => match self.receiver.recv_timeout(Duration::from_millis(100)) {
                     Ok(val) => self.registers.insert(instruction.register, val),
                     Err(_) => return sent,
                 },
-                Instructions::Jgz => {
+                Command::Jgz => {
                     if register_value > 0 {
-                        self.position = self.position + value - 1;
+                        self.position += value - 1;
                     }
                     None
                 }
